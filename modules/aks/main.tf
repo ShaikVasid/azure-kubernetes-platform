5f8b4e7ee410f7433b966148e4e8c6e37e3b1f12
ShaikVasid <98427286+ShaikVasid@users.noreply.github.com>
2026-08-22 22:07:40 -0400

    refactor: format AKS module resources

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -1,12 +1,3 @@
-variable "name" { type = string }
-variable "location" { type = string }
-variable "resource_group_name" { type = string }
-variable "subnet_id" { type = string }
-variable "kubernetes_version" { type = string default = null }
-variable "system_vm_size" { type = string default = "Standard_D2s_v5" }
-variable "user_vm_size" { type = string default = "Standard_D2s_v5" }
-variable "tags" { type = map(string) default = {} }
-
 resource "azurerm_kubernetes_cluster" "this" {
   name                = "${var.name}-aks"
   location            = var.location
@@ -14,7 +5,9 @@ resource "azurerm_kubernetes_cluster" "this" {
   dns_prefix          = "${var.name}-aks"
   kubernetes_version  = var.kubernetes_version
 
-  identity { type = "SystemAssigned" }
+  identity {
+    type = "SystemAssigned"
+  }
 
   azure_active_directory_role_based_access_control_enabled = true
   role_based_access_control_enabled                        = true
@@ -28,10 +21,10 @@ resource "azurerm_kubernetes_cluster" "this" {
   }
 
   network_profile {
-    network_plugin    = "azure"
+    network_plugin      = "azure"
     network_plugin_mode = "overlay"
-    network_policy    = "azure"
-    load_balancer_sku = "standard"
+    network_policy      = "azure"
+    load_balancer_sku   = "standard"
   }
 
   tags = var.tags
@@ -44,12 +37,10 @@ resource "azurerm_kubernetes_cluster_node_pool" "user" {
   vnet_subnet_id        = var.subnet_id
   node_count            = 2
   mode                  = "User"
+
   node_labels = {
     "workload-type" = "application"
   }
+
   tags = var.tags
 }
-
-output "cluster_id" { value = azurerm_kubernetes_cluster.this.id }
-output "cluster_name" { value = azurerm_kubernetes_cluster.this.name }
-output "kubelet_identity_object_id" { value = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id }
