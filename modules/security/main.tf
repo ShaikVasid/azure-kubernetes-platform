7a42309d33611dfd770e6a90a977a7414f7e957e
ShaikVasid <98427286+ShaikVasid@users.noreply.github.com>
2026-08-22 22:08:02 -0400

    refactor: format security module resources

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,13 +1,6 @@
-variable "name" { type = string }
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "tags" { type = map(string) default = {} }
-
 resource "azurerm_network_security_group" "aks" {
   name                = "${var.name}-aks-nsg"
   location            = var.location
   resource_group_name = var.resource_group_name
   tags                = var.tags
 }
-
-output "network_security_group_id" { value = azurerm_network_security_group.aks.id }
