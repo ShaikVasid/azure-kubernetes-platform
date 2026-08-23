aa9413d6bc58f6d8b9c857361c4e12b3c8072e9f
ShaikVasid <98427286+ShaikVasid@users.noreply.github.com>
2026-08-22 22:08:10 -0400

    refactor: centralize dev environment variables

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,11 +1,41 @@
 variable "location" {
-  description = "Azure region"
+  description = "Azure region where the AKS platform is deployed."
   type        = string
   default     = "canadacentral"
 }
 
 variable "resource_group_name" {
-  description = "Resource group for the AKS platform"
+  description = "Resource group name for the AKS platform."
   type        = string
   default     = "rg-portfolio-aks-dev"
+}
+
+variable "vnet_address_space" {
+  description = "CIDR address space for the AKS virtual network."
+  type        = string
+  default     = "10.30.0.0/16"
+}
+
+variable "aks_subnet_prefix" {
+  description = "CIDR prefix for the AKS subnet."
+  type        = string
+  default     = "10.30.0.0/22"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version for the AKS cluster."
+  type        = string
+  default     = null
+}
+
+variable "system_vm_size" {
+  description = "VM size for the AKS system node pool."
+  type        = string
+  default     = "Standard_D2s_v5"
+}
+
+variable "user_vm_size" {
+  description = "VM size for the AKS application node pool."
+  type        = string
+  default     = "Standard_D2s_v5"
 }
